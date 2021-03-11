3f9142c86ae552ea8167afb8eecad0597adac9d9
David Landes <dlandes.develop@outlook.com>
2021-03-11 12:50:45 -0500

    initial commit

---
+++ server.py
@@ -0,0 +1,43 @@
+import socket
+import threading
+
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+
+DISCONNECT_MESSAGE = "[DISCONNETED] client disconnected."
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        # Recieve header. Tells us how long of message we are about to recieve.
+        msg_len = conn.recv(HEADER).decode(FORMAT)
+        if msg_len:
+            msg_len = int(msg_len)
+            # Recieve message.
+            msg = conn.recv(msg_len).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+            print(f"[{addr}] {msg}")
+
+    conn.close()
+
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+print("[STARTING] server is starting...")
+start()
