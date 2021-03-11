bc8839ebe4f0aecc467c2be127f6b1e01b3b6973
David Landes <dlandes.develop@outlook.com>
2021-03-11 14:33:33 -0500

    Add basic client commands

--- client.py
+++ client.py
@@ -8,9 +8,30 @@ DISCONNECT_MESSAGE = "[DISCONNETED] client disconnected."
 SERVER = "192.168.1.21" #socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 
+SESSION_LIVE = True
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
+def try_cmd(cmd):
+    if type(cmd) == str:
+        if cmd == "quit" or cmd == "q":
+            print(f"[DISCONNECT] {SERVER} session has ended.")
+            return False
+
+        elif cmd[0:4] == "send" and cmd[4:5] == " ":
+            data = cmd[5:]
+            send(data)
+            return f""
+
+
+    print(f"Invalid command [{cmd}].")
+    return True
+
+def handle_input():
+    args = input(">>> ")
+    res = try_cmd(args)
+    return res
 
 def send(msg):
     message = msg.encode(FORMAT)
@@ -21,8 +42,12 @@ def send(msg):
     client.send(send_len)
     client.send(message)
 
-send("Hello world.")
-send("Hello everyone.")
-send("Hello tim.")
 
-send(DISCONNECT_MESSAGE)
+# Begin main loop.
+while SESSION_LIVE:
+    res = handle_input()
+    if type(res) == bool:
+        SESSION_LIVE = res
+    else:
+        print(f"{res}")
+
